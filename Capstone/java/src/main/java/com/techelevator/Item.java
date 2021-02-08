b3943f0185d28c368916d5f52f21ba51f7fa9ad8
Luke Rodman <luke.rodman5@gmail.com>
2021-02-08 08:33:46 -0500

    pushing what I have for capstone

--- Capstone/java/src/main/java/com/techelevator/Item.java
+++ Capstone/java/src/main/java/com/techelevator/Item.java
@@ -1,58 +1,48 @@
 package com.techelevator;
-
 public class Item {
+	
 	String itemName;
 	double itemPrice;
 	String itemSound;
 	String itemType;
 	int remainingStock;
-public Item(String itemName,double itemPrice,String itemType,int remainingStock) {
-	this.itemName = itemName;
-	this.itemPrice = itemPrice;
-	this.itemType = itemType;
-	this.remainingStock = remainingStock;
 	
-	if(this.itemType.equals("Candy")) {
-		this.itemSound = "Munch, Munch, Yum!";
+	public Item(String itemName,double itemPrice,String itemType,int remainingStock) {
+			this.itemName		= itemName;
+			this.itemPrice		= itemPrice;
+			this.itemType		= itemType;
+			this.remainingStock	= remainingStock;
+			
+				if(this.itemType.equals("Candy")) {			// create if statements to apply sounds to each item
+				this.itemSound 	= "Munch, Munch, Yum!";
+				}
+				if(this.itemType.equals("Chip")) {
+				this.itemSound	= "Crunch, Crunch, Yum!";
+				}
+				if(this.itemType.equals("Drink")) {
+				this.itemSound 	= "Glug, Glug, Yum!";
+				}
+				if(this.itemType.equals("Gum")) {
+				this.itemSound 	= "Chew, Chew, Yum!";
+				}
 	}
-	if(this.itemType.equals("Chip")) {
-		this.itemSound = "Crunch, Crunch, Yum!";
-	}
-	if(this.itemType.equals("Drink")) {
-		this.itemSound = "Glug, Glug, Yum!";
-	}
-	if(this.itemType.equals("Gum")) {
-		this.itemSound = "Chew, Chew, Yum!";
-	}
-}
-/**
- * @return the itemName
- */
-public String getItemName() {
-	return itemName;
-}
-/**
- * @return the itemPrice
- */
-public double getItemPrice() {
-	return itemPrice;
-}
-/**
- * @return the itemSound
- */
-public String getItemSound() {
-	return itemSound;
-}
-/**
- * @return the itemType
- */
-public String getItemType() {
-	return itemType;
-}
-/**
- * @return the remainingStock
- */
-public int getRemainingStock() {
-	return remainingStock;
-}
+	
+	public void setRemainingStock(int remainingStock) {		// @param remainingStock the remainingStock to set
+				this.remainingStock = remainingStock;
+				}
+	public String getItemName() {							// @return the itemName
+				return itemName;
+				}
+	public double getItemPrice(){							// @return the itemPrice
+				return itemPrice;
+				}
+	public String getItemSound(){							// @return the itemSound
+				return itemSound;
+				}
+	public String getItemType() {							// @return the itemType
+				return itemType;
+				}
+	public int getRemainingStock() {						// @return the remainingStock
+	 			return remainingStock;
+	 			}
 }
